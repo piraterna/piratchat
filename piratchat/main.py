5e40766f3ad46f2a45fd1b0fe01294a9d4fde13e
gaailiunas <gailiunaslaurynas@tutanota.com>
2025-02-27 21:40:14 +0200

    feat: routes.py

--- piratchat/main.py
+++ piratchat/main.py
@@ -1,33 +1,5 @@
-import aiohttp
 from aiohttp import web
-
-
-async def register(request: web.Request) -> web.Response:
-    json = await request.json()
-    print("json:", json)
-    return web.Response(text="hi")
-
-
-async def login(request: web.Request) -> web.Response:
-    json = await request.json()
-    print("json:", json)
-    return web.Response(text="hi")
-
-
-async def wshandler(request: web.Request) -> web.WebSocketResponse:
-    ws = web.WebSocketResponse()
-    await ws.prepare(request)
-
-    async for msg in ws:
-        print("ws data:", msg.data)
-        if msg.type == aiohttp.WSMsgType.TEXT:
-            await ws.send_str("hello world")
-        elif msg.type == aiohttp.WSMsgType.ERROR:
-            print(f"websocket connection closed with exception {ws.exception()}")
-
-    print("websocket connection closed")
-    return ws
-
+from routes import register, login, wshandler
 
 app = web.Application()
 app.add_routes(
