be2b53b898f2cba72ac96f82570234dcd637d827
gaailiunas <gailiunaslaurynas@tutanota.com>
2025-03-02 01:50:18 +0200

    prod code

--- piratchat/main.py
+++ piratchat/main.py
@@ -2,24 +2,18 @@ from aiohttp import web
 from routes import register, login, logout, wshandler
 from routes import handle_sessions
 
-
-'''
-async def index(request):
-    return web.FileResponse("test.html")
-'''
-
 app = web.Application()
+
 app.add_routes(
     [
         web.post("/register", register),
         web.post("/login", login),
         web.get("/logout", logout),
         web.get("/ws", wshandler),
-        #web.get("/", index),
     ]
 )
 
 app.on_startup.append(lambda app: handle_sessions())
 
 if __name__ == "__main__":
-    web.run_app(app)
+    web.run_app(app, host='0.0.0.0', port=7777)
