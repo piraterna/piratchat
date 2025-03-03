3ca9dd879fa2dbf268ef93cca023a2bef60c1dac
piraterna <github@piraterna.org>
2025-03-03 15:59:17 +0000

    fix: weird merge line removal

--- piratchat/main.py
+++ piratchat/main.py
@@ -6,19 +6,11 @@ app = web.Application()
 
 app.add_routes(
     [
-<<<<<<< HEAD
         web.post("/api/register", register),
         web.post("/api/login", login),
         web.get("/api/logout", logout),
         web.get("/api/online", get_online),
         web.get("/api/user/{username}", get_user),
-=======
-        web.post("/register", register),
-        web.post("/login", login),
-        web.get("/logout", logout),
-        web.get("/online", get_online),
-        web.get("/user/{username}", get_user),
->>>>>>> 99388879e988d7efc13d3a28b172d21eaea1bdb6
         web.get("/ws", wshandler),
     ]
 )
